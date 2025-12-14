d902922c5f67c8f63c69ed0208aae8b8c86a39a2
Mojocraft <281188392@qq.com>
2025-12-14 16:31:58 +0800

    add cmake

---
+++ build/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/link.d"
+  "CMakeFiles/hello.dir/test.c.o"
+  "CMakeFiles/hello.dir/test.c.o.d"
+  "hello"
+  "hello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/hello.dir/release.c.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/hello.dir/release.c.o: \
+  /data/data/com.termux/files/home/my_code/release.c
